6a73f2ecf03c6875f8b8b8d6343d352cd599556b
Kang Hyun Suk <khsfashi@naver.com>
2024-07-05 13:41:28 +0900

    오류 해결

--- Assets/Project/Scripts/ArcadeCarController.cs
+++ Assets/Project/Scripts/ArcadeCarController.cs
@@ -14,6 +14,8 @@ public class ArcadeCarController : MonoBehaviour
     [SerializeField] private Collider carCollider;
     [SerializeField] private Transform[] rayTransforms;
     [SerializeField] private LayerMask movableMask;
+    [SerializeField] private GameObject[] wheels = new GameObject[4];
+    [SerializeField] private GameObject[] frontWheelsParents = new GameObject[4];
     [SerializeField] private float wheelRadius;
 
     [Header("Suspension Physics Settings")]
@@ -31,6 +33,14 @@ public class ArcadeCarController : MonoBehaviour
     [SerializeField] private AnimationCurve turnCurve;
     [SerializeField] private float dragCoefficient = 1f;
 
+    [Header("VFX")]
+    [SerializeField] private float wheelRotationSpeed = 3000f;
+    [SerializeField] private float maxSteeringAngle = 30f;
+    [SerializeField] private TrailRenderer[] skidMarks = new TrailRenderer[2];
+    [SerializeField] private ParticleSystem[] skidEffects = new ParticleSystem[2];
+    [SerializeField] private float minSkidVelocity = 10f;
+
+
     [Header("Car Input")]
     private float moveInput = 0f;
     private float steerInput = 0f;
@@ -39,12 +49,15 @@ public class ArcadeCarController : MonoBehaviour
     private float velocityRatio = 0f;
     private Vector3 groundNormal = Vector3.zero;
 
+    private float currentSteerAngle = 0.0f;
+    private Vector3 wheelEulerAngles = Vector3.zero;
+
+    #region Unity
 
     private void Start()
     {
         // 인스펙터에서 넣어주는데, 혹시 까먹을까봐 안전장치용
         carRigidBody = GetComponent<Rigidbody>();
-        //AdjustCenterOfMass();
     }
 
     private void Update()
@@ -57,8 +70,11 @@ public class ArcadeCarController : MonoBehaviour
         CheckGrounded();
         CalculateVelocity();
         Movement();
+        VFX();
     }
+    #endregion
 
+    #region Movement
     private void Movement()
     {
         if (isGrounded)
@@ -82,7 +98,7 @@ public class ArcadeCarController : MonoBehaviour
 
     private void Turn()
     {
-        carRigidBody.AddTorque(steerStrength * steerInput * turnCurve.Evaluate(velocityRatio) * Mathf.Sign(velocityRatio) * transform.up, ForceMode.Acceleration);
+        carRigidBody.AddTorque(steerStrength * steerInput * turnCurve.Evaluate(Mathf.Abs(velocityRatio)) * Mathf.Sign(velocityRatio) * transform.up, ForceMode.Acceleration);
     }
 
     private void SidewaysDrag()
@@ -106,14 +122,16 @@ public class ArcadeCarController : MonoBehaviour
         steerInput = Input.GetAxis("Horizontal");
     }
 
-    private void AdjustCenterOfMass()
+    private void CalculateVelocity()
     {
-        Vector3 centerOfMass = carRigidBody.centerOfMass;
-        centerOfMass.y = -carCollider.bounds.extents.y - wheelRadius - extraLength;
-        carRigidBody.centerOfMass = centerOfMass;
-        Debug.DrawLine(transform.TransformPoint(centerOfMass), transform.TransformPoint(centerOfMass) + Vector3.up * 0.5f, Color.yellow, 100f);
+        currentLocalVelocity = transform.InverseTransformDirection(carRigidBody.linearVelocity);
+        velocityRatio = currentLocalVelocity.z / maxSpeed;
+        //Debug.Log(currentLocalVelocity.z * 3.6f + "km/h");
     }
 
+    #endregion
+
+    #region Check Car State
     private void CheckGrounded()
     {
         int groundedWheels = 0;
@@ -124,13 +142,79 @@ public class ArcadeCarController : MonoBehaviour
 
         isGrounded = groundedWheels > 1 ? true : false;
     }
+    #endregion
 
-    private void CalculateVelocity()
+    #region VFX
+
+    private void VFX()
     {
-        currentLocalVelocity = transform.InverseTransformDirection(carRigidBody.linearVelocity);
-        velocityRatio = currentLocalVelocity.z / maxSpeed;
+        WheelVFXs();
+        //SkidVFXs();
     }
 
+    private void SkidVFXs()
+    {
+        if (isGrounded && currentLocalVelocity.x > minSkidVelocity)
+        {
+            ToggleSkidMarks(true);
+            ToggleSkidEffects(true);
+        }
+        else
+        {
+            ToggleSkidMarks(false);
+            ToggleSkidEffects(false);
+        }
+    }
+
+    private void ToggleSkidMarks(bool toggle)
+    {
+        foreach(var skidMark in skidMarks)
+        {
+            skidMark.enabled = toggle;
+        }
+    }
+
+    private void ToggleSkidEffects(bool toggle)
+    {
+        foreach(var skidEffect in skidEffects)
+        {
+            if (toggle) skidEffect.Play();
+            else skidEffect.Stop();
+        }
+    }
+
+    private void WheelVFXs()
+    {
+        float targetSteeringAngle = maxSteeringAngle * steerInput;
+        float steerSmoothSpeed = 5.0f;
+        currentSteerAngle = Mathf.Lerp(currentSteerAngle, targetSteeringAngle, steerSmoothSpeed * Time.deltaTime);
+
+        for(int i = 0; i < wheels.Length; ++i)
+        {
+            if (i < 2)
+            {
+                wheels[i].transform.Rotate(Vector3.right, wheelRotationSpeed * velocityRatio * Time.deltaTime, Space.Self);
+                // 원래 벡터 생성해서 쓰다가 new 매프레임 하는 거 좀 성능 문제 생길 것 같아서 멤버 변수 통해서 바꿈
+                wheelEulerAngles = frontWheelsParents[i].transform.localEulerAngles;
+                wheelEulerAngles.y = currentSteerAngle;
+                frontWheelsParents[i].transform.localEulerAngles = wheelEulerAngles;
+            }
+            else
+            {
+                wheels[i].transform.Rotate(Vector3.right, wheelRotationSpeed * moveInput * Time.deltaTime, Space.Self);
+                wheels[i].transform.Rotate(Vector3.right, wheelRotationSpeed * velocityRatio * Time.deltaTime, Space.Self);
+            }
+        }
+    }
+
+    private void SetWheelPosition(GameObject wheel, Vector3 wheelPosition)
+    {
+        wheel.transform.position = wheelPosition; 
+    }
+
+    #endregion
+
+    #region Suspension
     private void Suspension()
     {
         for (int i = 0; i < rayTransforms.Length; ++i)
@@ -154,13 +238,19 @@ public class ArcadeCarController : MonoBehaviour
 
                 carRigidBody.AddForceAtPosition(force * rayTransforms[i].up, rayTransforms[i].position);
 
+                SetWheelPosition(wheels[i], hit.point + rayTransforms[i].up * wheelRadius);
+
                 Debug.DrawLine(rayTransforms[i].position, hit.point, Color.red);
             }
             else
             {
                 isGroundedWheels[i] = 0;
+
+                SetWheelPosition(wheels[i], rayTransforms[i].position - rayTransforms[i].up * maxLength);
+
                 Debug.DrawLine(rayTransforms[i].position, rayTransforms[i].position + (wheelRadius + maxLength) * -rayTransforms[i].up, Color.green);
             }
         }
     }
+    #endregion
 }
